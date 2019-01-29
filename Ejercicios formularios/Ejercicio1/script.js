740560dcadfb114580f0d954dcfdf2185d2dd39b
Sergio <32309472+sergjime@users.noreply.github.com>
2019-01-29 10:50:02 +0100

    Add files via upload

--- Ejercicios formularios/Ejercicio1/script.js
+++ Ejercicios formularios/Ejercicio1/script.js
@@ -1,3 +1,35 @@
 window.onload = function() {
-    alert('SCRIPT LISTO');
+    var parrafo = document.getElementById('paragraf');
 };
+
+function validateForm() {
+    var formulario = document.forms[0];
+    var tipos = [];
+    var valores = retornaValor(formulario, tipos);
+    document.getElementById('paragraf').innerHTML = "Continguts dels diferents camps: " + valores;
+    console.log(valores);
+    return false;
+}
+
+function retornaValor(formulario, tipos) {
+    var valores = [];
+    for (i = 0; i < formulario.elements.length; i++) {
+        var elemento = formulario.elements[i];
+        var tipo = elemento.type;
+        tipos.push(tipo);
+        if (tipos[i] == "text" || tipos[i] == "textarea") {
+            valores.push(elemento.value);
+        } else {
+            if (tipos[i] == "radio" || tipos[i] == "checkbox") {
+                if (elemento.checked) {
+                    valores.push(elemento.value);
+                }
+            } else {
+                if (tipos[i] == "select") {
+                    valores.push(elemento.options[elemento.selectedIndex].value);
+                }
+            }
+        }
+    }
+    return valores;
+}
