a5ba7b2cb9c5b65bf5af63bf470611c68190e91d
Sergio <32309472+sergjime@users.noreply.github.com>
2019-02-11 16:41:20 +0100

    Update script.js

--- Ejercicios BOM/Ejercicio2/script.js
+++ Ejercicios BOM/Ejercicio2/script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     let fecha = new Date();
     let parrafo = document.createElement("p");
-    // let horaActual = setTimeout(cambia(fecha), 1000);
+    let horaActual = setInterval(function(){ cambia(fecha); }, 1000);
     let textoHora = document.createTextNode(cambia(fecha));
     parrafo.appendChild(textoHora);
     document.body.appendChild(parrafo);
